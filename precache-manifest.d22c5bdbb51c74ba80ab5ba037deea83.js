acff41c8296d9bae98a60a6ea63a52892a63b2b2
Lari Mikael Orpana <lari.m.orpana@student.jyu.fi>
2019-09-10 22:55:06 +0300

    Updates

---
+++ precache-manifest.d22c5bdbb51c74ba80ab5ba037deea83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e0e250e8c8eac6db49106292f67661",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "9045bf68c7903bd971a2",
+    "url": "/lights-out/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "35ea10311da3095822db",
+    "url": "/lights-out/static/js/2.4614ac65.chunk.js"
+  },
+  {
+    "revision": "9045bf68c7903bd971a2",
+    "url": "/lights-out/static/js/main.b7026cc6.chunk.js"
+  },
+  {
+    "revision": "0dc462df6a8d21ebc631",
+    "url": "/lights-out/static/js/runtime~main.672f2070.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lights-out/static/media/logo.5d5d9eef.svg"
+  }
+]);
